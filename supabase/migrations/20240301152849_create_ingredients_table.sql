da069ae884ba14829949581bb37a19c8244c8365
Othavio <otaviodelimasousa@gmail.com>
2024-03-01 14:48:43 -0300

    add ingredients migration

---
+++ supabase/migrations/20240301152849_create_ingredients_table.sql
@@ -0,0 +1,8 @@
+create table if not exists ingredients (
+    id text default encode(uuid_send(uuid_generate_v4()::uuid),'base64') primary key,
+    name text not null,
+    created_at timestamp with time zone default now() not null,
+    updated_at timestamp with time zone default now() not null,
+    deleted_at timestamp with time zone
+);
+
