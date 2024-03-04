d163cd62239a50a1546a4cf69ca7b9b009a49435
Othavio <otaviodelimasousa@gmail.com>
2024-03-04 12:05:51 -0300

    add list ingredients page

--- app/ingredients/page.tsx
+++ app/ingredients/page.tsx
@@ -1,4 +1,13 @@
- const Ingredients = ()=>{
-    return <h1>Hello Ingredients</h1>
+import { services } from "@/domain/services";
+
+ const Ingredients = async ()=>{
+    const ingredients = await services().ingredients.list();
+    return(
+        ingredients?.map((ingredient)=>{
+            return (
+                <h1>{ingredient.name}</h1>
+            );
+        })
+    )
 }
 export default Ingredients;
--- domain/services/index.ts
+++ domain/services/index.ts
@@ -1,8 +1,16 @@
-import { SupabaseClient } from '@supabase/supabase-js'
-import * as ingredient from './ingredient'
+import { createClient } from '@supabase/supabase-js'
+import * as Ingredient from './ingredient'
 
-export const services = (supabase:SupabaseClient)=>({
-    ingredients:()=>({
-        list: ingredient.get(supabase)
-    })
-})
+export const services =  ()=>{
+    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!!)
+
+    const services = {
+        ingredients: {
+            list: async () => {
+                return await Ingredient.get(supabase);
+            }
+        }
+    }
+    return services;
+
+}
